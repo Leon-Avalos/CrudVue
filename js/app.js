205170d9f41704cf03d9329e42391c5e598fcbe4
Leon <heiko.avalos@gmail.com>
2019-02-19 22:33:30 -0500

    Añadido: Metodos añadir, eliminar, editar y el local storage

--- js/app.js
+++ js/app.js
@@ -1,6 +1,43 @@
 var app = new Vue({ 
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        titulo: 'Control de actividades',
+        actividades: [],
+        nuevaActividad: ''
+    },
+    methods: {
+        agregarActividad: function(){
+            this.actividades.push({
+                nombre: this.nuevaActividad,
+                estado: false
+            });
+            this.nuevaActividad = '';
+            localStorage.setItem('actividades',
+            JSON.stringify(this.actividades));
+        },
+        editarActividad: function(index){
+            if(this.actividades[index].estado == false){
+                this.actividades[index].estado = true;
+            }else{
+                this.actividades[index].estado = false;
+            }
+            localStorage.setItem('actividades',
+            JSON.stringify(this.actividades));
+            
+        },
+        eliminarActividad: function(index){
+            this.actividades.splice(index, 1);
+            localStorage.setItem('actividades',
+            JSON.stringify(this.actividades));
+        }
+    },
+    created: function(index){
+        var datosActividades = JSON.parse(localStorage.getItem('actividades'));
+        if(datosActividades === null){
+            this.actividades = [];
+        }else{
+            this.actividades = datosActividades;
+        }
+
     }
 });
