67df7e9a0df0f8d5a7de0fe123abab7e9118e1e6
AndresGarcia98 <garciaagudelod@gmail.com>
2019-02-20 10:02:58 -0500

    Se comento una parte del codigo

--- js/app.js
+++ js/app.js
@@ -1,41 +1,50 @@
-var app = new Vue({ 
+//Aca es donde se maneja todo el CRUD de vue.js
+var app = new Vue({
     el: '#app',
+    //Aca es donde esta los datos que el usuario guardo (el index, nombre y estado)
     data: {
         titulo: 'Control de actividades',
         actividades: [],
         nuevaActividad: ''
     },
     methods: {
-        agregarActividad: function(){
+        //Este método se encarga de guardar los elementos en la base de datos local
+        agregarActividad: function () {
             this.actividades.push({
                 nombre: this.nuevaActividad,
                 estado: false
             });
+            //Se vuelve a vaciar los elementos que se guardo
             this.nuevaActividad = '';
+            //Se guardar a los elementos en la base de datos local
             localStorage.setItem('actividades',
-            JSON.stringify(this.actividades));
-        },
-        editarActividad: function(index){
-            if(this.actividades[index].estado == false){
-                this.actividades[index].estado = true;
-            }else{
-                this.actividades[index].estado = false;
-            }
-            localStorage.setItem('actividades',
-            JSON.stringify(this.actividades));
-            
-        },
-        eliminarActividad: function(index){
-            this.actividades.splice(index, 1);
-            localStorage.setItem('actividades',
-            JSON.stringify(this.actividades));
-        }
+                JSON.stringify(this.actividades));
     },
-    created: function(index){
+    //Este método que se encarga de editar el estado de lo que se guardo en la base de datos local
+    editarActividad: function (index) {
+        if (this.actividades[index].estado == false) {
+            this.actividades[index].estado = true;
+        } else {
+            this.actividades[index].estado = false;
+        }
+        //Se guarda el elemennto editado en la base de datos local
+        localStorage.setItem('actividades',
+            JSON.stringify(this.actividades));
+
+    },
+    //Este método se encarga de eliminar un  dato que se guardo en la base de datos local
+    eliminarActividad: function (index) {
+        this.actividades.splice(index, 1);
+        localStorage.setItem('actividades',
+            JSON.stringify(this.actividades));
+    }
+},
+    //Esta función se encarga de cuando de nuevo se abra el navegador se crguen los datos que estaban guardaos anteriormente.
+    created: function (index) {
         var datosActividades = JSON.parse(localStorage.getItem('actividades'));
-        if(datosActividades === null){
+        if (datosActividades === null) {
             this.actividades = [];
-        }else{
+        } else {
             this.actividades = datosActividades;
         }
 
